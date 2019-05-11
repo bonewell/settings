de72b1717e1e0bb4ad49a88f7302b5b11f5533f3
Konstantin Kolodiy <bonewell@yandex.ru>
2019-05-11 11:40:24 +0300

    xmonad

---
+++ xmonad/xfce4-panel.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+sleep 4
+xfce4-panel --restart
+alsactl restore # turn on sound
---
+++ xmonad/xmonad-installer.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+sudo apt install -y cabal-install
+sudo apt install -y libx11-dev
+sudo apt install -y libxrandr-dev
+sudo apt install -y libxss-dev
+
+cabal update
+cabal install xmonad
+cabal install xmonad-contrib
+
+mkdir ~/.xmonad
+cp -r dir/* ~/.xmonad
+
+echo "Add scripts to session starting of xfce and"
+echo "reboot computer to start xmonad!"
