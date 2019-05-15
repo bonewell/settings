30f5a71771cdc987174fecb2a92201fb6edace59
Konstantin Kolodiy <kolodiy_kg@magnit.ru>
2019-05-15 08:20:23 +0300

    Improve vim settings

---
+++ vim/vim-plug-installer.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+sudo apt install -y vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+cp vimrc ~/.vimrc
